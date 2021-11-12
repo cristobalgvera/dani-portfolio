d77fdc13fb3b789622d55bb24c15aff43350f34e
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-11-12 03:32:07 -0300

    :sparkles: Added external icon on compamy name experience section

--- src/components/experience/atoms/ExperienceCompany.tsx
+++ src/components/experience/atoms/ExperienceCompany.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { FiExternalLink } from 'react-icons/fi';
 import { IExperience } from '../../../shared/types/experience/IExperience';
 
 interface ExperienceCompanyProps
@@ -12,11 +13,14 @@ export const ExperienceCompany: FC<ExperienceCompanyProps> = ({
     <div className="pt-2 pb-0.5 flex">
       <span className="uppercase font-bold text-2xl">{companyName}</span>
       {companyLink && (
-        <div className="inline-flex w-auto space-x-5 ml-2 mt-1">
-          <a href={companyLink} target="_blank" rel="noreferrer noopener">
-            <span>LINK</span>
-          </a>
-        </div>
+        <a
+          href={companyLink}
+          className="ml-3 pt-1"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FiExternalLink className="text-xl text-blue-500" />
+        </a>
       )}
     </div>
   );
